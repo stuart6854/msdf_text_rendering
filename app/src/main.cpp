756db0dbc4e4c3562a6be3ca176c96fc4b2f1c6a
Stuart Millman <stu.millman15@gmail.com>
2023-07-08 08:50:42 +0100

    Hello Triangle

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -7,6 +7,34 @@
 #include <format>
 #include <iostream>
 
+const std::string VertexShaderSource = R"(
+#version 330 core
+#extension GL_ARB_separate_shader_objects : enable
+
+layout (location = 0) in vec3 in_Pos;
+layout (location = 1) in vec3 in_Color;
+
+layout (location = 0) out vec3 out_Color;
+
+void main() {
+    gl_Position = vec4(in_Pos, 1.0);
+    out_Color = in_Color;
+}
+)";
+
+const std::string FragmentShaderSource = R"(
+#version 330 core
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(location = 0) in vec3 in_Color;
+
+layout(location = 0) out vec3 out_fragColor;
+
+void main() {
+    out_fragColor = in_Color;
+}
+)";
+
 template <typename... Args>
 void println(std::string_view fmt, Args&&... args)
 {
@@ -24,7 +52,7 @@ static void errorCallback(int error, const char* description)
     errprintln(description);
 }
 
-GLFWwindow* Initialise(std::int32_t width, std::int32_t height, std::string_view title)
+GLFWwindow* initialise(std::int32_t width, std::int32_t height, std::string_view title)
 {
     glfwSetErrorCallback(errorCallback);
 
@@ -65,7 +93,87 @@ GLFWwindow* Initialise(std::int32_t width, std::int32_t height, std::string_view
     return window;
 }
 
-void render() {}
+GLuint create_buffers()
+{
+    struct Vertex
+    {
+        glm::vec3 pos;
+        glm::vec3 color;
+    };
+
+    std::vector<Vertex> vertexData{
+        { { -0.5f, -0.5f, 0.0f }, { 1.0f, 0.0f, 0.0f } },
+        { { 0.5f, -0.5f, 0.0f }, { 0.0f, 1.0f, 0.0f } },
+        { { 0.5f, 0.5f, 0.0f }, { 0.0f, 0.0f, 1.0f } },
+    };
+
+    GLuint vao{};
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    GLuint vbo{};
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertexData.size(), vertexData.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, pos)));
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, color)));
+    glEnableVertexAttribArray(1);
+
+    return vao;
+}
+
+GLuint compile_shader(const char* shaderSource, GLenum shaderType)
+{
+    GLuint shaderHandle = glCreateShader(shaderType);
+
+    glShaderSource(shaderHandle, 1, &shaderSource, nullptr);
+    glCompileShader(shaderHandle);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shaderHandle, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderHandle, 512, nullptr, infoLog);
+        errprintln("Error while compiling shader: {}", infoLog);
+    }
+
+    return shaderHandle;
+}
+
+GLuint create_shader_program(const std::string& vertexSource, const std::string& fragmentSource)
+{
+    auto vertexShader = compile_shader(vertexSource.c_str(), GL_VERTEX_SHADER);
+    auto fragmentShader = compile_shader(fragmentSource.c_str(), GL_FRAGMENT_SHADER);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shaderProgram, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
+        errprintln("Error while linking shaders: {}", infoLog);
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
+
+void render(GLuint program, GLuint vao)
+{
+    glUseProgram(program);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
 
 void cleanup(GLFWwindow* window)
 {
@@ -80,7 +188,9 @@ int main(int argc, char** argv)
     Font font("fonts/OpenSans-Regular.ttf");
     //    Font font2("fonts/segoesc.ttf");
 
-    auto* window = Initialise(1280, 720, "MSDF Text Rendering");
+    auto* window = initialise(1280, 720, "MSDF Text Rendering");
+    auto program = create_shader_program(VertexShaderSource, FragmentShaderSource);
+    auto vao = create_buffers();
 
     while (!glfwWindowShouldClose(window))
     {
@@ -88,7 +198,7 @@ int main(int argc, char** argv)
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        render();
+        render(program, vao);
 
         glfwSwapBuffers(window);
     }
